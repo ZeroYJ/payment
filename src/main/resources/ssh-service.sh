a6f889e21e43a8688cf47c08ebab43bdc81df6df
obaw <996067226@qq.com>
2017-05-03 13:13:37 +0800

    update prod

--- src/main/resources/ssh-service.sh
+++ src/main/resources/ssh-service.sh
@@ -3,9 +3,9 @@ set timeout 10
 spawn ssh root@fuliaoyi.com
 expect "*yes/no" {send "yes\r";exp_continue}
 send "cd /opt\r"
-send "ps_pid=\$(ps -ef|grep paymentdev.jar|grep -v grep|awk '{print \$2}')\r"
+send "ps_pid=\$(ps -ef|grep payment.jar|grep -v grep|awk '{print \$2}')\r"
 send "kill \$ps_pid\r"
-send "rm paymentdev.log\r"
-send "nohup java -jar paymentdev.jar >paymentdev.log &\r"
+send "rm payment.log\r"
+send "nohup java -jar payment.jar >payment.log &\r"
 send "exit\r"
 expect eof
