aec05bee5c73a7ea591f95b6e3aa991b75a90c8b
obaw <996067226@qq.com>
2017-05-18 16:39:58 +0800

    update ssh

--- src/main/resources/ssh-service.sh
+++ src/main/resources/ssh-service.sh
@@ -5,7 +5,5 @@ expect "*yes/no" {send "yes\r";exp_continue}
 send "cd /opt\r"
 send "ps_pid=\$(ps -ef|grep paymentdev.jar|grep -v grep|awk '{print \$2}')\r"
 send "kill \$ps_pid\r"
-send "rm paymentdev.log\r"
-send "nohup java -jar paymentdev.jar >paymentdev.log &\r"
 send "exit\r"
 expect eof
